ddae7a3ab6c0d4c3f1bc191031ef51a3f7bd8127
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-02-25 22:05:49 -0600

    sun at 10

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//display SVG of holiday 
+//display _ _ _ with number of letters in holiday
+
+
+
+//Number of Guesses Remaining:10, counting down
+//Letters Already Guessed:
+//count up wins
+//count up loses
