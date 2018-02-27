22c6b137595fcf1c60e7217dd360484945342110
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-02-26 23:28:24 -0600

    monday night

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-//display SVG of holiday 
+//possible holidays
+
+var holidayNames = ['Valentines Day', 'Halloween', 'July 4', 'Christmas', 'Thanksgiving', 'St Patricks Day'];
+
+//possible SVGS (make objects)
+
+var valentinesDay = {
+    'image': img.src = './images/heart.svg',
+}
+
+document.onkeyup = function(event) {
+    // Determines which key was pressed.
+    var userGuess = event.key;
+}
+//computer choose SVG and name holiday -- outputs to user
+
+var computerGuess = holidayNames[Math.floor(Math.random() * holidayNames.length)];
+
 //display _ _ _ with number of letters in holiday
 
+//user can press any key to start
+//records Letters Already Guessed:
 
+//if user guesses correctly fill in display _ _ _ 
+//else Number of Guesses Remaining:10, counting down
 
-//Number of Guesses Remaining:10, counting down
-//Letters Already Guessed:
-//count up wins
-//count up loses
+//if user guesses correctly, count up wins
+//else count up loses
