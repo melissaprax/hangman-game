d0043a9d39781ef76b7555fd15330ec706b57e8c
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-02-28 18:28:08 -0600

    wed at 6:30

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
-//possible holidays
+   
+    //possible holidays
 
-var holidayNames = ['Valentines Day', 'Halloween', 'July 4', 'Christmas', 'Thanksgiving', 'St Patricks Day'];
+    let holidayNames = ['Valentines Day', 'Halloween', 'July 4', 'Christmas', 'Thanksgiving', 'St Patricks Day'];
 
-for (var i = 0; i < holidayNames.length; i++) {
-    console.log(holidayNames[i]);
-  }
+    for (var i = 0; i < holidayNames.length; i++) {
+        console.log(holidayNames[i]);
+      }
 
-//possible SVGS (make objects)
-
-var valentinesDay = {
-    'image': img.src = './images/heart.svg',
-    'lenth': "_ _ _ _ _ _ _ _ _ _"
-}
-
-//computer choose SVG and name holiday -- outputs to user
-
-var computerGuess = holidayNames[Math.floor(Math.random() * holidayNames.length)];
-
-//display _ _ _ with number of letters in holiday
-
-//user can press any key to start
-//records Letters Already Guessed:
-
-//if user guesses correctly fill in display _ _ _ 
-//else Number of Guesses Remaining:10, counting down
-
-//if user guesses correctly, count up wins
-//else count up loses
+    let holidayNames = [
+        {
+            name: 'Christmas',
+            length: 9,
+            imgPath: '../images/santa.svg'
+        },
+        {
+            name: 'St Patricks Day',
+            length: 15,
+            imgPath: '../images/clover.svg'
+        },
+        {
+            name: 'Valentines Day'
+            length: 14,
+            imgPath: '../images/heart.svg',
+        },
+        {
+            name: 'Halloween',
+            length: 9,
+            imgPath: '../images/pumpkin.svg'
+        },
+        {
+            name: 'July 4'
+            length: 
+        }
+        
+    
+    //possible SVGS and _ _ _ _ (make objects?)
+    
+    //computer choose SVG and name holiday -- outputs to user
+    
+    //let computerGuess = holidayNames[Math.floor(Math.random() * holidayNames.length)];
+    
+    //display _ _ _ with number of letters in holiday
+    
+    //user can press any key to start
+    //records Letters Already Guessed:
+    
+    //if user guesses correctly fill in display _ _ _ 
+    //else Number of Guesses Remaining:10, counting down
+    
+    //if user guesses correctly, count up wins
+    //else count up loses
