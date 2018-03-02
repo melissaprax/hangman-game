222dd9525389dcd9e915a784bd98704b24f459c5
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-03-02 17:20:06 -0600

    friday at 5:19

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
    
     //possible holidays
 
-    let holidayNames = ['Valentines Day', 'Halloween', 'July 4', 'Christmas', 'Thanksgiving', 'St Patricks Day'];
-
-    for (var i = 0; i < holidayNames.length; i++) {
-        console.log(holidayNames[i]);
-      }
-
     let holidayNames = [
         {
             name: 'Christmas',
             length: 9,
-            imgPath: '../images/santa.svg'
         },
         {
-            name: 'St Patricks Day',
-            length: 15,
-            imgPath: '../images/clover.svg'
-        },
-        {
-            name: 'Valentines Day'
-            length: 14,
-            imgPath: '../images/heart.svg',
+            name: 'St Patricks',
+            length: 11,
         },
         {
             name: 'Halloween',
             length: 9,
-            imgPath: '../images/pumpkin.svg'
         },
         {
-            name: 'July 4'
-            length: 
+            name: 'Thanksgiving',
+            length: 12,
         }
+    ]
+
+    for (let i = 0; i < holidayNames.length; i++) {
+            console.log(holidayNames[i]);
+          }
         
-    
-    //possible SVGS and _ _ _ _ (make objects?)
+    // set variables for wins
+
+        let wins = 0;
+        let losses = 0;
+        let guessesLeft = 10;
+
+    // for dashes
+
+        let dashGuess = [];
+          
     
     //computer choose SVG and name holiday -- outputs to user
+
+
+    //let pickHoliday = function() {
+    let computerGuess = holidayNames[Math.floor(Math.random() * holidayNames.length)];
+    console.log(computerGuess);
+    //document.getElementById("computer").innerHTML = computerGuess;
+    //}
     
-    //let computerGuess = holidayNames[Math.floor(Math.random() * holidayNames.length)];
-    
+
     //display _ _ _ with number of letters in holiday
     
     //user can press any key to start
+
+    document.onkeyup = function(event) {
+        // to determine which key was pressed.
+        let userGuess = event.key;
+        //outputs to site
+        document.getElementById("user").innerHTML = userGuess;
+    }  
     //records Letters Already Guessed:
+
+    
+
+
     
     //if user guesses correctly fill in display _ _ _ 
     //else Number of Guesses Remaining:10, counting down
     
     //if user guesses correctly, count up wins
+
+
     //else count up loses
