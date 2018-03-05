e1ca621edc306944052abbf17378bcd9e7d3a8b6
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-03-04 19:15:49 -0600

    sun at 7:15

--- js/main.js
+++ js/main.js
@@ -47,8 +47,8 @@
         document.getElementById("computer").innerHTML = dashGuess += '_ ';
       }
     
-
-    //let computerWord = computerGuess.name
+    
+   // let computerWord = computerGuess.name
     
     //user can press any key to start
     document.onkeyup = function(event) {
@@ -59,25 +59,29 @@
     
         if (computerGuess.name.indexOf(userGuess) != -1) {
           document.getElementById("computer").innerHTML = userGuess;
+          let guessIndex = computerGuess.name.indexOf(userGuess);
+          console.log(guessIndex);
         }
-    else {
-      console.log(false);
-   }
+   
 
    //fill in user's guess to  _ _ _ in specific index position 
    //records Letters Already Guessed:
+   
 
-   if (computerChoice.indexOf(userGuess) > -1) {
-    guessIndex = computerChoice.indexOf(userGuess);
-  }
+
+
+
+   //if (computerGuess.indexOf(userGuess) > -1) {
+  //  guessIndex = computerGuess.indexOf(userGuess);
+ // }
   
-console.log(guessIndex);
+//console.log(guessIndex);
 
-dashGuess[guessIndex] = userKey;
+//dashGuess[guessIndex] = userKey;
 
-dashGuessStr = dashGuess.join(" ");
+//dashGuessStr = dashGuess.join(" ");
 
-console.log(dashGuess);
+//console.log(dashGuess);
 
 
    //makings wins and losses go up
@@ -90,42 +94,4 @@ console.log(dashGuess);
 
 
 
-} //to end event DON'T DELETE 
-
-    //if else Christmas 
-
-    // if (computerGuess === 'Christmas' && userGuess === 'c', 'h', 'r', 'i', 's', 't', 'm', 'a', 's') {
-    //     document.getElementById("wins").innerHTML = wins + 1;
-    // }
-    // else {
-    //     document.getElementById('loses').innerHTML = loses - 1;
-    // }
-    // //St Patricks
-    // if (computerGuess === 'St Patricks' && userGuess === 's', 't', 'p', 'a', 't', 'r', 'i', 'c', 'k', 's') {
-    //     document.getElementById("wins").innerHTML = wins + 1;
-    // }
-    // else {
-    //     document.getElementById('loses').innerHTML = loses - 1;
-    // }
-    //if user guesses a letter correctly guesses go unchanged
-
-    //if not guesses goes down by 1
-
-    //if guesses go down by 10, loses go up by 1
-
-    //if user guesses all letters correctly, wins go up by one
-
-    //display _ _ _ with number of letters in holiday
-    
-
-    
-
-
-    
-    //if user guesses correctly fill in display _ _ _ 
-    //else Number of Guesses Remaining:10, counting down
-    
-    //if user guesses correctly, count up wins
-
-
-    //else count up loses
+} 
