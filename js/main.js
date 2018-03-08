f52dd9775ed0564918e04b46cc50eefb3474016e
Melissa Prax <melissaprax@Melissas-MacBook-Pro.local>
2018-03-08 16:51:29 -0600

    thurs at 4:50

--- js/main.js
+++ js/main.js
@@ -1,117 +1,115 @@
    
 //possible holidays
 
-let emojiNames = [
-    {
-        name: 'smile',
-        length: 5,
-        pic: '../images/smile.png'
-    },
-    {
-        name: 'heart',
-        length: 5,
-        pic: '../images/heart.png'
-    },
-    {
-        name: 'fire',
-        length: 4,
-        pic: '../images/fire.png'
-    },
-    {
-        name: 'dog',
-        length: 3,
-        pic: '../images/dog.png'
-    },
-    {
-        name: 'flag',
-        length: 4,
-        pic: '../images/flag.png'
-    },
-    {
-        name: 'clap',
-        length: 4,
-        pic: '../images/clap.png'
-    },
-    {
-        name: 'cat',
-        length: 3,
-        pic: '../images/cat.png'
-    }
-        
-]
-   
-        // set variables for wins, losses and guesses 
-        let wins = 0;
-        let losses = 0;
-        
-        // variable for dashes
-        let dashGuess = [];
-
-        // set variables to get elements from HTML 
-        let idComputer = document.getElementById("computer");
-        let idUser = document.getElementById("user");
-        let idWins = document.getElementById("wins");
-        let idLosses = document.getElementById("losses");
-        let idGuesses = document.getElementById("guesses");
-
-        //computer choose name holiday 
-        let computerGuess = emojiNames[Math.floor(Math.random() * emojiNames.length)].name;
-            console.log(computerGuess);
-        
-
-        let guessesLeft = computerGuess.length +3;
-
-        //computer outputs _ _ _ length of holiday it chose
-        for (let i = 0; i < computerGuess.length; i++) {
-            //this is outputting as a number, not with underscores...
-            idComputer.innerHTML = dashGuess.push("_")
+    let emojiNames = [
+        {
+            name: 'smile',
+            length: 5,
+            pic: '../images/smile.png'
+        },
+        {
+            name: 'heart',
+            length: 5,
+            pic: '../images/heart.png'
+        },
+        {
+            name: 'fire',
+            length: 4,
+            pic: '../images/fire.png'
+        },
+        {
+            name: 'dog',
+            length: 3,
+            pic: '../images/dog.png'
+        },
+        {
+            name: 'flag',
+            length: 4,
+            pic: '../images/flag.png'
+        },
+        {
+            name: 'clap',
+            length: 4,
+            pic: '../images/clap.png'
+        },
+        {   
+            name: 'cat',
+            length: 3,
+            pic: '../images/cat.png'
         }
-
-        //output _ _ _ to HTML 
-
-        idComputer.innerHTML = dashGuess.join(" ")
         
-        //user can press any key to start
+    ]
+   
+    // set variables for wins, losses and guesses 
+    let wins = 0;
+    let losses = 0;
 
-        document.onkeyup = function userTry (event) {
-            if (guessesLeft > 0) {
+    // variable for dashes
+    let dashGuess = [];
 
-            
-            // to determine which key was pressed.
-            let userGuess = event.key.toLowerCase();
-            guessesLeft--;
-            console.log(guessesLeft) 
-            idGuesses.innerHTML = guessesLeft
+    // set variables to get elements from HTML 
+    let idComputer = document.getElementById("computer");
+    let idUser = document.getElementById("user");
+    let idWins = document.getElementById("wins");
+    let idLosses = document.getElementById("losses");
+    let idGuesses = document.getElementById("guesses");
 
-            //outputs to site
-            idUser.innerHTML = userGuess;
+    //computer choose name holiday 
+    let computerGuess = emojiNames[Math.floor(Math.random() * emojiNames.length)].name;
+        console.log(computerGuess);
+        
+    let guessesLeft = computerGuess.length +3;
+
+    //computer outputs _ _ _ length of holiday it chose
+    function computerLoop () {
+        for (let i = 0; i < computerGuess.length; i++) {
+
+    //this is outputting as a number, not with underscores...
+    idComputer.innerHTML = dashGuess.push("_")
+        }
+    }
+
+    let tryThis = computerLoop(emojiNames);
+
+    //output _ _ _ to HTML 
+
+    idComputer.innerHTML = dashGuess.join(" ")
+        
+    //user can press any key to start
+    document.onkeyup = function userTry (event) {
+        if (guessesLeft > 0) {
+
+    // to determine which key was pressed.
+    let userGuess = event.key.toLowerCase();
+        guessesLeft--;
+        console.log(guessesLeft) 
+        idGuesses.innerHTML = guessesLeft
+
+    //outputs to site
+        idUser.innerHTML = userGuess;
 
             if (computerGuess.indexOf(userGuess) > -1) {
                 console.log("correct guess")
+                
                 //change dash guess to show to correct letter in the correct position
                 let correctIndex = computerGuess.indexOf(userGuess);
                 dashGuess[correctIndex] = userGuess;
                 console.log(dashGuess)
                 //counter--;
                 //console.log(counter)
-                if (dashGuess.join("") === computerGuess) {
-                    forWins();
-                    endGame();
-                }
-                idComputer.innerHTML = dashGuess.join(" ")
-            } else {
-                console.log("incorrect")
+                    if (dashGuess.join("") === computerGuess) {
+                        forWins();
+                        endGame();
+                    }
+                        idComputer.innerHTML = dashGuess.join(" ")
+                    } else {
+                        console.log("incorrect")
+                    }
             }
-        }
-        else {
-            countDown ();
+            else {
+                countDown ();
             // restart game
-            endGame();
-
-            // set a new word
-            // reset guesses left
-
-            //reset after win and loss
+                endGame();
 
         }
         }
@@ -119,27 +117,27 @@ let emojiNames = [
 //once the word has 0 letters left, then wins and losses can go up / down accordingly
 //need to get losses to update in console after guesses run out
 //need to get wins to update in console after counter runs out
-        function countDown () {
-            if (guessesLeft === 0) {
+    function countDown () {
+        if (guessesLeft === 0) {
             console.log("losses" + losses++)
             idLosses.innerHTML = losses++
-            } 
-        }
+        } 
+    }
     
 
-         function forWins () {
-        
-             console.log("wins " + wins++)
-             idWins.innerHTML = wins++
+    function forWins () {
+        console.log("wins " + wins++)
+        idWins.innerHTML = wins++
              
          }
 
-         function endGame () {
-            computerGuess = emojiNames[Math.floor(Math.random() * emojiNames.length)].name;
-            guessesLeft = computerGuess.length +3;
-            dashGuess = [];
+    function endGame () {
+        computerGuess = emojiNames[Math.floor(Math.random() * emojiNames.length)].name;
+        guessesLeft = computerGuess.length +3;
+        dashGuess = [];
+        tryThis = computerLoop(computerGuess);
            
-         }
+    }
 
 
 
